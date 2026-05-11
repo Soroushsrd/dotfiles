73cce79ccc72a1e3e925eaf97171ccdac748ea88
Soroushsrd <Sardashtisoroosh@gmail.com>
2026-05-11 09:51:25 +0330

    [fix]: gc-threshold and native-comp-speed to improve startup perf

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -2,11 +2,9 @@
 ;; Silence native comp warning pop ups(logged to *Warnings*)
 (setq native-comp-async-report-warnings-errors 'silent)
 (setq package-enable-at-startup nil)
-(setq gc-cons-threshold most-positive-fixnum
-      gc-cons-percentage 0.6)
+(setq gc-cons-threshold most-positive-fixnum)
 (add-hook 'emacs-startup-hook
-          (lambda () (setq gc-cons-threshold (* 64 1024 1024)
-                           gc-cons-percentage 0.1)))
+          (lambda () (setq gc-cons-threshold (* 64 1024 1024))))
 (setq inhibit-startup-message t
       inhibit-startup-screen t
       initial-scratch-message nil)
@@ -17,3 +15,5 @@
 (scroll-bar-mode -1)
 ;; Frame defaults (so they apply before the first frame draws)
 (add-to-list 'default-frame-alist '(alpha-background . 0.90))
+(setq native-comp-speed 2)
+
