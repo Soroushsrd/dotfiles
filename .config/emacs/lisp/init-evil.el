83f5151db431be6f4250d956525e0af9fd9f9f98
Soroushsrd <Sardashtisoroosh@gmail.com>
2026-07-31 23:49:07 +0330

    add(keybinding): turn up diagnostics with flycheck

--- .config/emacs/lisp/init-evil.el
+++ .config/emacs/lisp/init-evil.el
@@ -44,7 +44,7 @@
   "SPC" '(execute-extended-command :which-key "M-x")
   "f f"   '(find-file :which-key "dired")
   "/"   '(consult-ripgrep :which-key "search project")
-  "e"    '(flymake-show-buffer-diagnostics :which-key "buffer diagnostics")
+  "e"    '(consult-flycheck :which-key "diagnostics")
 
   ;; ClaudeCode
 
